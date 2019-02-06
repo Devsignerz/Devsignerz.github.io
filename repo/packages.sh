2252bd1b634d908c0b02b1fa0d299f67b3d33b9a
Devsignerz  <Devsignerz@gmail.com>
2019-02-06 01:24:13 -0600

    init

---
+++ repo/packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dpkg-deb -bZgzip projects/iOS9Theme
+dpkg-deb -bZgzip projects/iOS10LBI
+dpkg-deb -bZgzip projects/iOS11Theme
+dpkg-deb -bZgzip projects/photosFix
+dpkg-deb -bZgzip projects/Serendipity
+dpkg-deb -bZgzip projects/noBetaAlert
